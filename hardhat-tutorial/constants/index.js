12758123423e93065ab63700ea8b6edc63cc6cf1
Adeleke Anuoluwapo <102337992+cryptanu@users.noreply.github.com>
2022-05-24 10:17:21 +0100

    combined with NFT collection Dapp

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x6082917d81fc8a671f26ADBB9F1e58Ea7F223b24";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
